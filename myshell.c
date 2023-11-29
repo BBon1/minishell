0a32bab82fda0e122f53983aa07fab21d1ad940a
Boni <100412444+BBon1@users.noreply.github.com>
2023-11-29 18:21:37 +0100

    Update myshell.c
    Intento de redirecciones

--- myshell.c
+++ myshell.c
@@ -11,7 +11,7 @@
 
 //  VARIABLES GLOBALES
 pid_t * hijos;  // Array dinamico de pids
-int * tub; // Array dinámico de los pipes
+tub * tube; // Array dinámico de los pipes
 tline * line; // Array dinamico de tlines
 int m; // Variable para indicar que mandato toca ejecutar
 
@@ -20,9 +20,9 @@ void manejador_mandatos ();
 int main() {
 	// Poner todas las declaraciones de variables locales al principio del main (o funciones)
 	char buf[1024];
-	int i; // Variable para los for
 	char check_exit [] = "exit\n";
 	pid_t pid;	
+	int i;
 	
 	signal(SIGUSR1, manejador_mandatos); // Minishell llama al manejador para ejecutar mandatos
 	signal(2 ,SIG_IGN);  // Mini shell ignora Ctrl+C
@@ -33,12 +33,13 @@ int main() {
 		if (strcmp(check_exit, buf) == 0){ // Escribir exit para poder salir de la minishell
 			printf("Saliendo de la minishell...\n");
 			// Liberar memoria dinamica, cerrar pipes...
-			free(line);
+			free(tube);
 			free(hijos); 
+			// CLOSE PIPES
 			return 0;
 			
 		} else if (strcmp("\n", buf) == 0){ // Escribir exit para poder salir de la minishell
-			printf("==> ");
+			printf("minishell ==> ");
 			continue;
 		}
 		
@@ -50,18 +51,28 @@ int main() {
 		}
 		
 		// Crear los hijos necesarios
-		hijos = (pid_t*)calloc(line->ncommands, sizeof(pid_t));
-		for (m = 0 ; m < line->ncommands; m ++){ //Crear un hijo para cada mandato, la variable global m cambia de valor al final del for
+		hijos = (pid_t*)calloc (line->ncommands, sizeof(pid_t));
+		
+		tube = (tub *)calloc(line->ncommands-1, sizeof(tub));
+		for (i = 0; i < line->ncommands; i++){
+			pipe(tube[i].tuberia);
+		}
+		
+		for (m = 0 ; m < line->ncommands; m ++){ //Crear un hijo para cada mandato, la variable global m cambia de valor al final del for	
+			
 			pid = fork();
 			if (pid == 0){
 				pause(); //Dejar esperandolos hasta recibir una señal
 			} else {
 				hijos[m] = pid;
+				close(tube[m].tuberia[0]);
+				close(tube[m].tuberia[1]);
 			}
 		}
 		
 		// Despertar el primer hijo/mandato (ultimo)
 		kill(hijos[line->ncommands-1], SIGUSR1);
+		
 		
 		// La minishell solo espera bloqueada si se ejecuta en primer plano, y solo espera al ultimo mandato
 		if ( !(line->background) ){ 
@@ -82,15 +93,17 @@ int main() {
 
 void manejador_mandatos (){
 	int file_input, file_output, file_error;  //Ficheros utilizados si hay redirecciones de input, output o error
+	int i ;
 	
 	signal(2 ,SIG_DFL); // Los hijos si mueren con ctrl+c
 	
-	if (m == line->ncommands-1){ // Ultimo hijo utimo mandato m
-		kill(hijos[m-1], SIGUSR1); // Despertar al resto de hijos
-
-		//  REDIRECCION DE PIPES POR AQUI s
-		// dup2(pipe[m-1][1], 1);  pipe  -> salida std 
-		sleep(m); // APAÑO
+	if ( line->ncommands != 1 && m == line->ncommands-1 ){ // Ultimo hijo utimo mandato m, distinto de 0
+	
+		//  REDIRECCION DE PIPES / entrada
+		
+		close (tube[m-1].tuberia[1]); 
+		dup2(tube[m-1].tuberia[0], 0); // pipe  -> entrada 
+		
 		
 		// Redirecciones de output y error
 		if (line->redirect_output != NULL) {
@@ -103,29 +116,58 @@ void manejador_mandatos (){
 			file_error = open(line->redirect_error , O_WRONLY | O_CREAT, 0666);
 			dup2(file_error,2);
 		}
-		// Ejecutar mandato
-		execvp(line->commands[m].filename , line->commands[m].argv);
-		fprintf(stderr, "Error al ejecutar el programa\n");
-		exit(1);
-	}else if (m > 0){ //Mandato intermedios Hijo - (m,0)
+	
+	
 		kill(hijos[m-1], SIGUSR1); // Despertar al resto de hijos
 		
-		//  REDIRECCION DE PIPES POR AQUI	
-		// dup2(pipe[m+1][1], 0);  pipe  -> salida std 
-		sleep(i); // APAÑO
 		
 		// Ejecutar mandato
 		execvp(line->commands[m].filename , line->commands[m].argv);
 		fprintf(stderr, "Error al ejecutar el programa\n");
 		exit(1);
 		
-	} else { // Primer hijo, primer mandato - Hijo 0
-	
+	}else if (line->ncommands != 1 && m > 0){ //Mandato intermedios Hijo - (m,0)
+		kill(hijos[m-1], SIGUSR1); // Despertar al resto de hijos
+		
+		//  REDIRECCION DE PIPES POR AQUI
+		
+		close (tube[m-1].tuberia[1]);	
+		close (tube[m].tuberia[0]);
+		dup2(tube[m-1].tuberia[0], 0); // pipe lectura -> stdin
+		dup2(tube[m].tuberia[1], 1); //  pipe salida -> stdout
+		
+		
+		// Ejecutar mandato
+		execvp(line->commands[m].filename , line->commands[m].argv);
+		fprintf(stderr, "Error al ejecutar el programa\n");
+		exit(1);
+		
+	} else { // Primer hijo, primer mandato - Hijo 0 / Un solo hijo
+		
+		//  REDIRECCION DE PIPES
+		close(tube[m].tuberia[0]); // cerrar el pipe[0]
+		if (line->ncommands > 1){  // Si hay mas de 1 mandato, redireccionar salida
+			dup2(tube[m].tuberia[1], 1); // pipe[0][1]  -> salida 
+			
+		} else {
+			// Redirecciones de output y error
+			if (line->redirect_output != NULL) {
+				file_output = open(line->redirect_output , O_WRONLY | O_CREAT, 0666); 
+				// Permisos resultantes -> 664, porque está umask 002
+				// No permite escritura del archivo a todo el mundo, se puede cambiar el umask
+				dup2(file_output ,1);
+			}
+			if (line->redirect_error != NULL) {
+				file_error = open(line->redirect_error , O_WRONLY | O_CREAT, 0666);
+				dup2(file_error, 2);
+			}
+		}
 		//Redirección input
 		if (line->redirect_input != NULL){
 			file_input = open(line->redirect_input , O_RDONLY);
 			dup2(file_input ,0);
 		}
+		
 		// Ejecutar mandato
 		execvp(line->commands[m].filename , line->commands[m].argv);
 		fprintf(stderr, "Error al ejecutar el programa\n");
